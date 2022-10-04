91bfc335113003c12ab52f2ffa67ded381727e4b
Kehinde Onifade <onifkay@gmail.com>
2022-10-04 20:35:09 +0100

    Create Action Types

---
+++ src/redux/Stock.js
@@ -0,0 +1,9 @@
+// Action Types
+const GET_STOCK_DATA = 'stocks-market-trends/stocks/GET_STOCK_DATA';
+const GET_COMPANY_DETAILS = 'stocks-market-trends/stocks/GET_COMPANY_DETAILS';
+const GET_COMPANY_STATEMENTS = 'stocks-market-trends/stocks/GET_COMPANY_STATEMENTS';
+const FILTER_COMPANY = 'stocks-market-trends/stocks/FILTER_COMPANY';
+const RESET_STOCK = 'stocks-market-trends/stocks/RESET_STOCK';
+const API_URL = 'https://financialmodelingprep.com/api/v3/';
+const API_KEY = "82a4de9376f4c4d59d47b606435ee1e1";
+
