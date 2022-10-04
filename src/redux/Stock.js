7addc97ad1fa349df3663b49761a76b8a3841ef7
Kehinde Onifade <onifkay@gmail.com>
2022-10-04 20:37:05 +0100

    Create stock actions

--- src/redux/Stock.js
+++ src/redux/Stock.js
@@ -14,3 +14,27 @@ const initialState = {
     statement: [],
     filtered: [],
   };
+
+  export const getStockData = (payload) => ({
+    type: GET_STOCK_DATA,
+    payload,
+  });
+  
+  export const getCompanyDetails = (payload) => ({
+    type: GET_COMPANY_DETAILS,
+    payload,
+  });
+  
+  export const getCompanyStatement = (payload) => ({
+    type: GET_COMPANY_STATEMENTS,
+    payload,
+  });
+  
+  export const filterCompany = (payload) => ({
+    type: FILTER_COMPANY,
+    payload,
+  });
+  
+  export const resetStock = () => ({
+    type: RESET_STOCK,
+  });
