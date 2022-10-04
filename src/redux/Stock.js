effcf9d78b31b55437a245e4a6e672b63e0358b1
Kehinde Onifade <onifkay@gmail.com>
2022-10-04 20:50:29 +0100

    get company details

--- src/redux/Stock.js
+++ src/redux/Stock.js
@@ -1,40 +1,52 @@
 // Action Types
-const GET_STOCK_DATA = 'stocks-market-trends/stocks/GET_STOCK_DATA';
-const GET_COMPANY_DETAILS = 'stocks-market-trends/stocks/GET_COMPANY_DETAILS';
-const GET_COMPANY_STATEMENTS = 'stocks-market-trends/stocks/GET_COMPANY_STATEMENTS';
-const FILTER_COMPANY = 'stocks-market-trends/stocks/FILTER_COMPANY';
-const RESET_STOCK = 'stocks-market-trends/stocks/RESET_STOCK';
-const API_URL = 'https://financialmodelingprep.com/api/v3/';
+const GET_STOCK_DATA = "stocks-market-trends/stocks/GET_STOCK_DATA";
+const GET_COMPANY_DETAILS = "stocks-market-trends/stocks/GET_COMPANY_DETAILS";
+const GET_COMPANY_STATEMENTS = "stocks-market-trends/stocks/GET_COMPANY_STATEMENTS";
+const FILTER_COMPANY = "stocks-market-trends/stocks/FILTER_COMPANY";
+const RESET_STOCK = "stocks-market-trends/stocks/RESET_STOCK";
+const API_URL = "https://financialmodelingprep.com/api/v3/";
 const API_KEY = "82a4de9376f4c4d59d47b606435ee1e1";
 
 // Stock actions
 const initialState = {
-    stocksData: [],
-    details: [],
-    statement: [],
-    filtered: [],
-  };
+  stocksData: [],
+  details: [],
+  statement: [],
+  filtered: [],
+};
 
-  export const getStockData = (payload) => ({
-    type: GET_STOCK_DATA,
-    payload,
-  });
-  
-  export const getCompanyDetails = (payload) => ({
-    type: GET_COMPANY_DETAILS,
-    payload,
-  });
-  
-  export const getCompanyStatement = (payload) => ({
-    type: GET_COMPANY_STATEMENTS,
-    payload,
-  });
-  
-  export const filterCompany = (payload) => ({
-    type: FILTER_COMPANY,
-    payload,
-  });
-  
-  export const resetStock = () => ({
-    type: RESET_STOCK,
-  });
+export const getCompanyDetails = (payload) => ({
+  type: GET_COMPANY_DETAILS,
+  payload,
+});
+
+export const getCompanyStatement = (payload) => ({
+  type: GET_COMPANY_STATEMENTS,
+  payload,
+});
+
+export const filterCompany = (payload) => ({
+  type: FILTER_COMPANY,
+  payload,
+});
+
+export const getStockData = (payload) => ({
+  type: GET_STOCK_DATA,
+  payload,
+});
+
+export const resetStock = () => ({
+  type: RESET_STOCK,
+});
+
+export const fetchCompanyDetails = (companyId) => async (dispatch) => {
+    try {
+      const response = await fetch(
+        `${API_URL}profile/${companyId}?apikey=${API_KEY}`,
+      );
+      const result = await response.json();
+      dispatch(getCompanyDetails(result));
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
